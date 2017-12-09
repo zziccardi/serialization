4e666509b5efa65955681b4c5abc1d561959bb24
Zach Ziccardi <zziccardi@users.noreply.github.com>
2017-12-08 23:04:18 -0500

    Adding filled-in AllMyTypes classes

--- genericCheckpointing/src/genericCheckpointing/util/MyAllTypesFirst.java
+++ genericCheckpointing/src/genericCheckpointing/util/MyAllTypesFirst.java
@@ -3,7 +3,60 @@ package genericCheckpointing.util;
 
 public class MyAllTypesFirst extends SerializableObject {
     
+    private int     myInt;
+    private int     myOtherInt;
+    private long    myLong;
+    private long    myOtherLong;
+    private String  myString;
+    private boolean myBool;
     
+    public void setMyInt(int i) {
+        myInt = i;
+    }
+    
+    public int getMyInt() {
+        return myInt;
+    }
+    
+    public void setMyOtherInt(int i) {
+        myOtherInt = i;
+    }
+    
+    public int getMyOtherInt() {
+        return myOtherInt;
+    }
+    
+    public void setMyLong(long l) {
+        myLong = l;
+    }
+    
+    public long getMyLong() {
+        return myLong;
+    }
+    
+    public void setMyOtherLong(long l) {
+        myOtherLong = l;
+    }
+    
+    public long getMyOtherLong() {
+        return myOtherLong;
+    }
+    
+    public void setMyString(String s) {
+        myString = s;
+    }
+    
+    public String getMyString() {
+        return myString;
+    }
+    
+    public void setMyBool(boolean b) {
+        myBool = b;
+    }
+    
+    public boolean getMyBool() {
+        return myBool;
+    }
     
 }
 
--- genericCheckpointing/src/genericCheckpointing/util/MyAllTypesSecond.java
+++ genericCheckpointing/src/genericCheckpointing/util/MyAllTypesSecond.java
@@ -3,7 +3,60 @@ package genericCheckpointing.util;
 
 public class MyAllTypesSecond extends SerializableObject {
     
+    private double myDoubleT;
+    private double myOtherDoubleT;
+    private float  myFloatT;
+    private short  myShortT;
+    private short  myOtherShortT;
+    private char   myCharT;
     
+    public void setMyDoubleT(double d) {
+        myDoubleT = d;
+    }
+    
+    public double getMyDoubleT() {
+        return myDoubleT;
+    }
+    
+    public void setMyOtherDoubleT(double d) {
+        myOtherDoubleT = d;
+    }
+    
+    public double getMyOtherDoubleT() {
+        return myOtherDoubleT;
+    }
+    
+    public void setMyFloatT(float f) {
+        myFloatT = f;
+    }
+    
+    public float getMyFloatT() {
+        return myFloatT;
+    }
+    
+    public void setMyShortT(short s) {
+        myShortT = s;
+    }
+    
+    public short getMyShortT() {
+        return myShortT;
+    }
+    
+    public void setMyOtherShortT(short s) {
+        myOtherShortT = s;
+    }
+    
+    public short getMyOtherShortT() {
+        return myOtherShortT;
+    }
+    
+    public void setMyCharT(char c) {
+        myCharT = c;
+    }
+    
+    public char getMyCharT() {
+        return myCharT;
+    }
     
 }
 
